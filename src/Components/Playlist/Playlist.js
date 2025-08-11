b5951f0258dd51c136c122dd0d349b72c628915e
JVaulter <jeroencluydts1@gmail.com>
2025-08-11 13:16:04 +0200

    Add trackUris and handleSaveToSpotify function to Playlist component

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -5,7 +5,18 @@ import SaveToSpotifyButton from "../Buttons/SaveToSpotifyButton";
 export const Playlist = ({ playlist, removeTrack }) => {
     const [name, setName] = useState(playlist.name);
 
+    const trackUris = [
+        "7kqhFEqZvl9ynxwD8No4qM",
+        "2kKMxha1iB9SSosC7S3AXF",
+        "6px340oxL4x4RrhLer462m",
+        "3S1fsqAI2wseDq6JcpamoY",
+        "6GsP3uMCd0Dn5T37C93waZ"
+    ]
 
+    const handleSaveToSpotify = () => {
+        // Logic to save the playlist to Spotify using the trackUris
+
+    }
 
     return (
         <div>
